1b1d920b9d0d057b14f6ed1094df6bfa61e422a5
Prabhjeet Singh <prabhjeet93cs@gmail.com>
2022-10-03 23:04:34 -0400

    no message

--- Basic_Projects/automation/excelreads.py
+++ Basic_Projects/automation/excelreads.py
@@ -1,21 +1,46 @@
 import pandas as pd
 import csv
+from time import sleep
+from selenium  import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 
 
 
-# df_sheet_index = pd.read_excel('./automation/data.xlsx')
-# #print(df_sheet_index)
-# print(type(df_sheet_index))
+# # writing the csv file line by line
+# with open('data1.csv','w', newline='') as f:
+#     wr=csv.writer(f)
+#     #wr.writerow(["URLs"])
+#     wr.writerow(["https://prabhjeetlearning.medium.com/linux-commands1-51b225d80364"])
+#     wr.writerow(["https://prabhjeetlearning.medium.com/linux-commands1-51b225d80364"])
+#     wr.writerow(["https://prabhjeetlearning.medium.com/linux-commands1-51b225d80364"])
 
-# print(df_sheet_index.count())
+# f.close()
 
-# for i in df_sheet_index.count:
-#     print("The link is = ",i)
+# with open('data1.csv', 'r') as file:
+#     rd = csv.reader(file)
+#     print(type(rd))
+#     for row in rd:
+#         print(row)
+# file.close()
+def writeTocsv(URLs):
+    with open('data1.csv','w', newline='') as f:
+        wr=csv.writer(f)
+        wr.writerow([URLs])
 
-with open('./automation/data.csv', 'r') as file:
-    rd = csv.reader(file)
-    print(type(rd))
-    # for row in rd:
-    #     print(row)
-file.close()
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver = webdriver.Chrome(executable_path=r'./tools/chromedriver', options=options)
+
+driver.maximize_window()
+print("Navigating to the page")
+
+driver.get("https://prabhjeetlearning.medium.com/")
+driver.implicitly_wait(20)
+
+elems = driver.find_elements(By.XPATH,"//a[@href]")
+
+for elem in elems:
+    url = elem.get_attribute("href")
+    writeTocsv(url)
