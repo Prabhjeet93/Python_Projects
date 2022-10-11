8a8d7a74b6b5b9e5b58e46a3bccdf0745d7902cb
Prabhjeet Singh <prabhjeet93cs@gmail.com>
2022-10-10 22:20:28 -0400

    Run urls with curl command.

---
+++ Basic_Projects/urlwithshellscript.py
@@ -0,0 +1,19 @@
+# """
+# How to run URL in terminal with the help of curl command
+# """
+
+
+
+# url=["www.google.com","www.youtube.com", "www.facebook.com"]
+
+# # set these values first in terminal
+# set urls(url)
+# echo $urls
+
+# for u in $urls                                                                                                    57.1s  Mon 10 Oct 2022 10:15:16 PM EDT
+#          curl $u --proxy http://localhost:8080/
+# end
+
+# # It will execute urls mentions in urls list in your browser who proxy is set.
+# # This demo is part of ZAP + web Pentest
+
