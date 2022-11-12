89e43b2a2b6a2e5ed7475bbc12e2891868e02b2c
Prabhjeet Singh <prabhjeet93cs@gmail.com>
2022-11-12 12:26:45 -0500

    This code will run Windows commands in Python

--- Basic_Projects/cmd_commands.py
+++ Basic_Projects/cmd_commands.py
@@ -1,11 +1,27 @@
 import os
+import sys
 
-# str = "python for absolute beginners"
-# print (str.capitalize())
+""" This code is run Windows commands in Python."""
 
-# str="TURN OFF LINK LIGHTS IN PACKET TRACER FOR THIS LAB"
-# print(str.lower())
 
+# get current directory
 print(os.getcwd())
+
+# Change to different  directory
+#os.chdir("C:/test")
+print(os.getcwd())
+
 file_name=os.path.join(os.getcwd(), '/data.csv')
 print(os.path.realpath('data.csv'))
+
+# print windows platform with version
+print(sys.platform)
+
+# list of directory names that constitute the current search path
+print(sys.path) 
+
+print(os.name)
+print(os.error)
+print(os.listdir())
+print(os.mkdir("test11"))
+print(os.rmdir("test11"))
