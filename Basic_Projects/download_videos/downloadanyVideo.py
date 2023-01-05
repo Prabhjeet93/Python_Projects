fccce9d1d00bad734b4e1c8d187d3f3de17bd211
Prabhjeet Singh <prabhjeet93cs@gmail.com>
2023-01-04 19:37:21 -0500

    no message

---
+++ Basic_Projects/download_videos/downloadanyVideo.py
@@ -0,0 +1,16 @@
+import requests
+
+url = 'video url'
+
+response = requests.get(url)
+
+with open('video.mp4', 'wb') as f:
+    f.write(response.content)
+
+
+
+import urllib.request
+
+url = 'video url'
+
+urllib.request.urlretrieve(url, 'video.mp4')
